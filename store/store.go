b21ec515769b98d939620bf119f6dba69e7cc456
Bart Lute <bart.lute@gmail.com>
2025-08-20 15:42:13 +0200

    - A fix for when the expiry is set to None (got cleaned because of epoch was set) - A small adjustment on paste insertion in an attempt to make UTC consistent

--- store/store.go
+++ store/store.go
@@ -1,39 +1,39 @@
 package store
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
-    "os"
-    "time"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
-    _ "github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Store interface {
-    GetPaste(id string) (Paste, error)
-    SavePaste(id string, contents string, encrypted bool, expireAt string) error
+	GetPaste(id string) (Paste, error)
+	SavePaste(id string, contents string, encrypted bool, expireAt string) error
 }
 
 type db struct {
-    ctx *sql.DB
+	ctx *sql.DB
 }
 
 type Paste struct {
-    Hash    string
-    Content string
-    Hashed  bool
+	Hash    string
+	Content string
+	Hashed  bool
 }
 
 func New() Store {
-    dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
-    ctx, err := sql.Open("mysql", dsn)
-    if err != nil {
-        log.Fatal(err)
-    }
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+	ctx, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    init := []string{
-        `CREATE TABLE IF NOT EXISTS pastes (
+	init := []string{
+		`CREATE TABLE IF NOT EXISTS pastes (
             id INT NOT NULL AUTO_INCREMENT,
             paste_id TEXT,
             content TEXT,
@@ -44,47 +44,47 @@ func New() Store {
             PRIMARY KEY (ID)
         )
         `,
-    }
-    for _, statement := range init {
-        _, err = ctx.Exec(statement)
-        if err != nil {
-            log.Fatal(err)
-        }
-    }
+	}
+	for _, statement := range init {
+		_, err = ctx.Exec(statement)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
-    return &db{
-        ctx: ctx,
-    }
+	return &db{
+		ctx: ctx,
+	}
 }
 
 func (d db) GetPaste(id string) (Paste, error) {
-    stmt, err := d.ctx.Prepare("SELECT content, encrypted FROM pastes WHERE paste_id = ?")
-    if err != nil {
-        return Paste{}, err
-    }
-    defer stmt.Close()
+	stmt, err := d.ctx.Prepare("SELECT content, encrypted FROM pastes WHERE paste_id = ?")
+	if err != nil {
+		return Paste{}, err
+	}
+	defer stmt.Close()
 
-    var contents string
-    var encrypted bool
-    err = stmt.QueryRow(id).Scan(&contents, &encrypted)
-    if err != nil {
-        return Paste{}, err
-    }
+	var contents string
+	var encrypted bool
+	err = stmt.QueryRow(id).Scan(&contents, &encrypted)
+	if err != nil {
+		return Paste{}, err
+	}
 
-    return Paste{
-        Content: contents,
-        Hashed:  encrypted,
-    }, nil
+	return Paste{
+		Content: contents,
+		Hashed:  encrypted,
+	}, nil
 }
 
 func (d db) SavePaste(id string, content string, encrypted bool, expireAt string) error {
-    _, err := d.ctx.Exec(
-        `INSERT INTO pastes (paste_id, content, encrypted, expire_at, created_at) VALUES(?, ?, ?, ?, ?)`,
-        id,
-        content,
-        encrypted,
-        expireAt,
-        time.Now().Format("2006-01-02T15:04:05"),
-    )
-    return err
+	_, err := d.ctx.Exec(
+		`INSERT INTO pastes (paste_id, content, encrypted, expire_at, created_at) VALUES(?, ?, ?, ?, ?)`,
+		id,
+		content,
+		encrypted,
+		expireAt,
+		time.Now().UTC().Format("2006-01-02T15:04:05"), // UTC for consistency
+	)
+	return err
 }
